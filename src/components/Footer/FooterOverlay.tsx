52f0759834bb5c34c8cca8475f036d56c17dd0b4
Kingsley Eneja <kingsley.eneja@getir.com>
2023-01-24 13:08:10 +0100

    Fix overlay bug

--- src/components/Footer/FooterOverlay.tsx
+++ src/components/Footer/FooterOverlay.tsx
@@ -1,9 +1,10 @@
-import React from 'react'
+import './FooterOverlay.css';
 
-const FooterOverlay = () => {
-  return (
-    <div>FooterOverlay</div>
-  )
-}
+const FooterOverlay = () => (
+  <div className='app__footerOverlay'>
+    <div className='app__footerOverlay-black' />
+    <div className='app__footerOverlay-img app__bg' />
+  </div>
+);
 
-export default FooterOverlay
+export default FooterOverlay;
